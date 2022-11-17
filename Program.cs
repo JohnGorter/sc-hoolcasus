638e2af24f68862523a469dbda03c504c0a1ac40
John Gorter <john.gorter@gmail.com>
2022-11-17 14:44:41 +0100

    STAP 3. Implementeer de sequence diagram en pas deze aan aan de inzichten van deze code

--- Program.cs
+++ Program.cs
@@ -1,32 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Goal goal = new Goal() { Description = "PROGRAMMEREN"};
-Goal goal2 = new Goal() { Description = "REFLECTEREN"};
-Goal goal3 = new Goal() { Description = "PRESENTEREN"};
-Goal goal4 = new Goal() { Description = "NIETRELEVANT"};
+
+
+// STAP 2. Voorbereiden van de data 
+Goal goal = new Goal() { Description = "PROGRAMM"};
+Goal goal2 = new Goal() { Description = "REFLECT"};
+Goal goal3 = new Goal() { Description = "PRESENT"};
+Goal goal4 = new Goal() { Description = "IRRELEVANT"};
 
 Outcome outcome = new Outcome() { Goals = new List<Goal>(), Description = "PROFESSIONAL DEVELOPER "};
 outcome.Goals.Add(goal); 
 outcome.Goals.Add(goal2); 
 outcome.Goals.Add(goal3); 
 
-Lesson lesson = new Lesson() { Description="Develop a game", Goals= new List<Goal>() } ;
-lesson.Goals.Add(goal); 
-Lesson lesson2 = new Lesson() { Description="Presenteren", Goals = new List<Goal>() };
-lesson.Goals.Add(goal3); 
-Lesson lesson3 = new Lesson() { Description="Onzinles", Goals = new List<Goal>() };
-lesson.Goals.Add(goal4); 
+Outcome outcome2 = new Outcome() { Goals = new List<Goal>(), Description = "NONSENS QUALIFICATION"};
+outcome2.Goals.Add(goal4); 
 
-Exam e = new Exam() { Goals = new List<Goal>(), Code = "SToets1" };
-e.Goals.Add(goal); 
+Lesson lesson = new Lesson() { Description="Develop a game", Goal = goal} ;
+Lesson lesson2 = new Lesson() { Description="Present a lesson", Goal = goal3};
+Lesson lesson3 = new Lesson() { Description="Bullshitlesson", Goal = goal4};
+
+Exam e = new Exam() {Code = "SExam1", Goal = goal };
 
 Teacher teacher = new Teacher() { Name = "John "};
 Course course = new Course() { Name="OOSE", Teacher = teacher, Outcomes = new List<Outcome>(), Lessons = new List<Lesson>(), Exams = new List<Exam>()};
-course.Lessons.AddRange(new List<Lesson>() { lesson, lesson2, lesson3});
+course.Outcomes.AddRange(new List<Outcome>{ outcome });
+course.Lessons.AddRange(new List<Lesson>() { lesson, lesson2});
 course.Exams.AddRange(new List<Exam>() { e });
+
+// STAP 3. Sequence diagram overnemen en testen
+Console.WriteLine($"Is the course consistent? {course.Validate()}"); 
 
 
 // STAP 1. Overnemen van het klassediagram in code om te kijken hoe het "voelt" 
-
 class Person {
     public String? Name { get; set; }
 }
@@ -42,7 +47,28 @@ class Course {
     public List<Lesson>? Lessons { get; set; } 
     public List<Exam>? Exams { get; set; } 
 
-    public bool Validate(){ return true;}
+    public bool Validate(){ 
+        bool valid = true; 
+        List<Goal> courseGoals = new List<Goal>(); 
+        if (Outcomes != null) foreach (Outcome outcome in Outcomes) { courseGoals.AddRange(outcome.getGoals()); }
+        foreach(Lesson lesson in Lessons!){
+           if(!lesson.Validate(courseGoals)) { 
+            System.Console.WriteLine("Lesson " + lesson.Description + " is serving a goal that is not for this course (" + lesson.Goal!.Description + ")");
+            valid = false;
+            break;
+           }
+        }
+        if (valid) {
+            foreach(Exam exam in Exams!){
+                if(!exam.Validate(courseGoals)) { 
+                    System.Console.WriteLine("Exam " + exam.Code + " is serving a goal that is not for this course (" + exam.Goal!.Description + ")");
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        return valid; 
+    }
     public List<Goal> getGoals() { return new List<Goal>();}
 
 }
@@ -55,19 +81,23 @@ class Outcome {
     public String? Description { get; set; }
     public List<Goal>? Goals { get; set; }
 
-    public List<Goal>? getGoals() { return new List<Goal>();}
+    public List<Goal> getGoals() { return Goals!;}
 }
 
 class Lesson {
     public String? Description { get; set; }
-    public List<Goal>? Goals { get; set; }
+    public Goal? Goal { get; set; }
 
-    public bool Validate() { return true; }
+    public bool Validate(List<Goal> courseGoals) { 
+        return (courseGoals.IndexOf(Goal!) >= 0); // op dit moment kijken we echt naar object identiteit, niet naar code of titel van een goal..
+     }
 }
 
 class Exam {
     public String? Code { get; set; }
-    public List<Goal>? Goals { get; set; }
+    public Goal? Goal { get; set; }
 
-    public bool Validate() { return true; }
+    public bool Validate(List<Goal> courseGoals) { 
+        return (courseGoals.IndexOf(Goal!) >= 0); // op dit moment kijken we echt naar object identiteit, niet naar code of titel van een goal..
+    }
 }
