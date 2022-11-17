c9c0494f35cdb1f8a6f84c6ab51ad42049efc902
John Gorter <john.gorter@gmail.com>
2022-11-17 13:45:27 +0100

    stap2: voorbereiden

--- Program.cs
+++ Program.cs
@@ -1,22 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Goal goal = new Goal() { Description = "PROGRAMMEREN"};
+Goal goal2 = new Goal() { Description = "REFLECTEREN"};
+Goal goal3 = new Goal() { Description = "PRESENTEREN"};
+Goal goal4 = new Goal() { Description = "NIETRELEVANT"};
+
+Outcome outcome = new Outcome() { Goals = new List<Goal>(), Description = "PROFESSIONAL DEVELOPER "};
+outcome.Goals.Add(goal); 
+outcome.Goals.Add(goal2); 
+outcome.Goals.Add(goal3); 
+
+Lesson lesson = new Lesson() { Description="Develop a game", Goals= new List<Goal>() } ;
+lesson.Goals.Add(goal); 
+Lesson lesson2 = new Lesson() { Description="Presenteren", Goals = new List<Goal>() };
+lesson.Goals.Add(goal3); 
+Lesson lesson3 = new Lesson() { Description="Onzinles", Goals = new List<Goal>() };
+lesson.Goals.Add(goal4); 
+
+Exam e = new Exam() { Goals = new List<Goal>(), Code = "SToets1" };
+e.Goals.Add(goal); 
+
+Teacher teacher = new Teacher() { Name = "John "};
+Course course = new Course() { Name="OOSE", Teacher = teacher, Outcomes = new List<Outcome>(), Lessons = new List<Lesson>(), Exams = new List<Exam>()};
+course.Lessons.AddRange(new List<Lesson>() { lesson, lesson2, lesson3});
+course.Exams.AddRange(new List<Exam>() { e });
+
 
 // STAP 1. Overnemen van het klassediagram in code om te kijken hoe het "voelt" 
 
 class Person {
-    public String Name { get; set; }
+    public String? Name { get; set; }
 }
 
 class Teacher : Person {
-    public List<Course> Courses { get; set; }
+    public List<Course>? Courses { get; set; }
 }
 
 class Course {
-    public Teacher teacher { get; set;}
-    public String Name { get; set; }
-    public List<Outcome> Outcomes { get; set; }
-    public List<Lesson> Lessons { get; set; } 
-    public List<Exam> Exams { get; set; } 
+    public Teacher? Teacher { get; set;}
+    public String? Name { get; set; }
+    public List<Outcome>? Outcomes { get; set; }
+    public List<Lesson>? Lessons { get; set; } 
+    public List<Exam>? Exams { get; set; } 
 
     public bool Validate(){ return true;}
     public List<Goal> getGoals() { return new List<Goal>();}
@@ -24,26 +48,26 @@ class Course {
 }
 
 class Goal {
-    public String Description { get; set; }
+    public String? Description { get; set; }
 }
 
 class Outcome {
-    public String Description { get; set; }
-    public List<Goal> Goals { get; set; }
+    public String? Description { get; set; }
+    public List<Goal>? Goals { get; set; }
 
-    public List<Goal> getGoals() { return new List<Goal>();}
+    public List<Goal>? getGoals() { return new List<Goal>();}
 }
 
 class Lesson {
-    public String Description { get; set; }
-    public List<Goal> Goals { get; set; }
+    public String? Description { get; set; }
+    public List<Goal>? Goals { get; set; }
 
     public bool Validate() { return true; }
 }
 
 class Exam {
-    public String Code { get; set; }
-    public List<Goal> Goals { get; set; }
+    public String? Code { get; set; }
+    public List<Goal>? Goals { get; set; }
 
     public bool Validate() { return true; }
 }
